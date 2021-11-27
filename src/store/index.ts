1e144f21e686b33524279f0052d7ae0628a7fa63
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-11-27 00:24:51 -0500

    Added redux logging for non-browser redux debugging

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from 'store/reducers/index';
 import rootSaga from 'store/sagas';
 
 const sagaMiddleware = createSagaMiddleware();
+const loggerMiddleware = createLogger();
+
 const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(sagaMiddleware),
+  applyMiddleware(sagaMiddleware, loggerMiddleware),
 ));
 
 sagaMiddleware.run(rootSaga);
